a47c5f673e7e1bed9131c4b20986e3ef54e6c0ac
Juan I. Carlis <juancarlis93@gmail.com>
2022-02-14 17:51:08 -0300

    first try outs

--- app/main.py
+++ app/main.py
@@ -1,4 +1,10 @@
-from fastapi import FastAPI
+import json
+
+from typing import Optional
+
+from fastapi import FastAPI, status, Query
+
+from models import Album
 
 
 app = FastAPI()
@@ -7,3 +13,20 @@ app = FastAPI()
 @app.get(path='/')
 def home():
     return {'Test API': 'Hello World!'}
+
+
+@app.get('/api/json/')
+def get_json():
+
+    with open('data.json', 'r', encoding='utf-8') as f:
+
+        results = json.loads(f.read())
+
+        return results
+
+
+@app.get('/api/json/albums')
+def show_album(
+        q: Optional[str] = Query(None)
+):
+    return q
